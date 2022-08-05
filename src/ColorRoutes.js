67b513e5da67c825a4f79d76f14ca68e1089b7e0
Your Name <you@example.com>
2022-08-04 21:45:20 -0400

    new commit

---
+++ src/ColorRoutes.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import {  Route, BrowserRouter ,Routes ,Navigate } from "react-router-dom";
+
+import ColorList from "./ColorList";
+import NewColorForm from "./NewColorForm";
+import Color from "./Color";
+
+
+function ColorRoutes() {
+
+  const initialColors = JSON.parse(localStorage.getItem("colors")) || {
+    red: "#FF0000",
+    green: "#00FF00",
+    blue: "#0000FF"
+  };
+  const [colors, updateColors] = useState(initialColors);
+
+  useEffect(
+    () => localStorage.setItem("colors", JSON.stringify(colors)),
+    [colors]
+  );
+
+  function handleAdd(newColorObj) {
+    updateColors(prevColors => ({ ...prevColors, ...newColorObj }));
+  }
+
+  function renderCurrentColor(props) {
+    const { color } = props.match.params;
+    const hex = colors[color];
+    return <Color {...props} hex={hex} color={color} />;
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/colors" element= {<ColorList/>} >
+          <ColorList colors={colors} />
+        </Route>
+
+        <Route exact path="/colors/new" element = {<NewColorForm/>}>
+          <NewColorForm addColor={handleAdd} />
+        </Route>
+
+        <Route path="/colors/:color" render={renderCurrentColor} element = {<Navigate to = "/colors" replace/>}/>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default ColorRoutes;
